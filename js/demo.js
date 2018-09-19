62c85508420de49652dfad6a976cb23dd31306c9
xiao <530102537@qq.com>
2018-09-19 09:15:40 +0800

    da

--- js/demo.js
+++ js/demo.js
@@ -2,7 +2,7 @@ var oInput = document.getElementsByTagName('input')[0];
 var oUl = document.getElementsByTagName('ul')[0];
 var oBox = document.getElementsByClassName('box')[0];
 var oSBtn = document.getElementById('search-btn');
-
+var oA = oSBtn.getElementsByTagName('a')[0];
 oInput.oninput = debounce(getUserAction, 150, false);
 
 function getUserAction() {
@@ -57,7 +57,28 @@ function doFunc(data) {
     }
 }
 oSBtn.onclick = function () {
-    var oA = oSBtn.getElementsByTagName('a')[0];
     var value = oInput.value;
     oA.href = 'https://www.baidu.com/s?wd=' + value;
+}
+
+function hidden() {
+    oBox.style.display = 'none';
+}
+oInput.onblur = hidden;
+oBox.onmousemove = function () {
+    oBox.style.display = 'block';
+    oInput.onblur = '';
+}
+oBox.onmouseout = function () {
+    oInput.onblur = hidden;
+}
+oInput.onfocus = function () {
+    oBox.style.display = 'block';
+}
+oInput.onkeydown = function (e) {
+    var code = e.keyCode;
+    if (code == '13') {
+        var value = oInput.value;
+        window.location = 'https://www.baidu.com/s?wd=' + value;
+    }
 }
